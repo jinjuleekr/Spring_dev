48b89b0d850a63f8f7bab114b7003ff8cb9326ab
jinjuleekr <jinju2415@gmail.com>
2019-01-10 17:11:49 +0900

    update dependency setting in pom.xml

--- Lively/src/main/java/com/quadcore/Lively/SSHConnection.java
+++ Lively/src/main/java/com/quadcore/Lively/SSHConnection.java
@@ -1,4 +1,4 @@
-package com.quadcore.Lively;
+/*package com.quadcore.Lively;
 
 import com.jcraft.jsch.ChannelExec;
 import com.jcraft.jsch.JSch;
@@ -49,7 +49,7 @@ import java.util.Properties;
 	            channel.connect();
 	            System.out.println("doInBackground: 시작 " + channel.isConnected() + ", " + channel.isClosed() + ", " + channel.getExitStatus());
 	            //doInBackground: 여기 시작 true, false, -1
-	            while (channel.isConnected()) { /** TODO : 이 부분 해결해야한다. */ // 참고 : channel.getExitStatus() != 1
+	            while (channel.isConnected()) { *//** TODO : 이 부분 해결해야한다. *//* // 참고 : channel.getExitStatus() != 1
 	                try {
 	                    Thread.sleep(1000); // 최소 1초 이상 필요
 	                    System.out.println("doInBackground: " + baos.toString()); // 명령에 대한 결과 호출 (반드시 Thread.sleep 이후에 작동)
@@ -72,3 +72,4 @@ import java.util.Properties;
 	}
 	
 }
+*/
